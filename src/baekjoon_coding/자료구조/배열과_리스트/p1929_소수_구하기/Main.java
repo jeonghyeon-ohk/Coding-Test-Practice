7911d4e2150fd180a30c1c32d3d811eeda9bd311
sulee <ok6737@naver.com>
2023-10-26 02:11:55 +0900

    1929_소수구하기_20231026

---
+++ src/baekjoon_coding/자료구조/배열과_리스트/p1929_소수_구하기/Main.java
@@ -0,0 +1,38 @@
+package baekjoon_coding.자료구조.배열과_리스트.p1929_소수_구하기;
+import java.util.*;
+import java.io.*;
+public class Main{
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int m = Integer.parseInt(st.nextToken());
+    int n = Integer.parseInt(st.nextToken());
+
+    boolean isPrime[] = new boolean[n+1];
+
+    isPrime[1] = false;
+    for(int i=2;i<isPrime.length; i++)
+      isPrime[i] = true;
+
+    for(int i=2;i<=Math.sqrt(n);i++){
+      if(isPrime[i]){
+        for(int j=i+i; j<=n; j+=i)
+          isPrime[j] = false;
+      }
+    }
+
+    for(int i=m; i<=n; i++){
+      if(isPrime[i])
+        sb.append(i).append("\n");
+    }
+
+    bw.append(sb);
+
+    br.close();
+    bw.close();
+
+  }
+}
